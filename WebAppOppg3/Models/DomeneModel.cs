bfe3eda19aa6e7145b039594486bc6fdfef54561
renkjet <47675663+renkjet@users.noreply.github.com>
2019-11-05 09:13:44 -0800

    .

--- WebAppOppg3/Models/DomeneModel.cs
+++ WebAppOppg3/Models/DomeneModel.cs
@@ -17,12 +17,18 @@ namespace WebAppOppg3.Models
 
         }
 
-        public class InnsendtSpmDomene
-        {
-            public int Id { get; set; }
-            public String Navn { get; set; }
-            public String Epost { get; set; }
-            public String Sporsmal { get; set; }
+    public class InnsendtSpmDomene
+    {
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression("^[a-zæøåA-ZÆØÅ. \\-]{2,50}$")]
+        public String Navn { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")]
+        public String Epost { get; set; }
+        [Required]
+        [RegularExpression("^[a-zøæåA-ZØÆÅ.0-9!?.,;.() \\-]{2,250}$")] // Hvor mange tegn????
+        public String Sporsmal { get; set; }
             
         }
     }
